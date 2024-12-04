a2b96f02acb7083ceae20b542f5db3f699cd41fa
DOMARICU <95487748+DOMARICU@users.noreply.github.com>
2024-12-04 23:10:32 +0100

    Create ui.lua

---
+++ ui.lua
@@ -0,0 +1,45 @@
+local createui = {}
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+function createui.start()
+  local Window = Rayfield:CreateWindow({
+    Name = "War Tycoon Script",
+    Icon = 0,
+    LoadingTitle = "1.2.5",
+    LoadingSubtitle = "by Furabyte",
+    Theme = "Default",
+  
+    DisableRayfieldPrompts = false,
+    DisableBuildWarnings = true,
+  
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = "furabyte",
+       FileName = "WarTycoon"
+    },
+  
+    Discord = {
+       Enabled = false,
+       Invite = "noinvitelink",
+       RememberJoins = true
+    },
+  
+    KeySystem = false, -- Set this to true to use our key system
+    KeySettings = {
+       Title = "Untitled",
+       Subtitle = "Key System",
+       Note = "No method of obtaining the key is provided",
+       FileName = "Key",
+       SaveKey = true,
+       GrabKeyFromSite = false,
+       Key = {"Hello"}
+    }
+  })
+  
+  local MAINTAB = Window:CreateTab("MAIN", "warehouse")
+  local VISUALSTAB = Window:CreateTab("VISUALS", "crosshair")
+  local RAGETAB = Window:CreateTab("RAGE", "swords")
+  local INFOTAB = Window:CreateTab("INFO", "info")
+end
+
+return createui
